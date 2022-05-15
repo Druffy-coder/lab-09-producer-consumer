af210bb71c51aaf12d98894685b9657284494dad
svsv <uzr68448@cuoly.com>
2022-05-16 01:43:02 +0300

    Refresh_1

--- tests/example.cpp
+++ tests/example.cpp
@@ -1,7 +1,6 @@
 // Copyright 2021 Your Name <your_email>
 
 #include <stdexcept>
-#include <thread>
 
 #include <gtest/gtest.h>
 
@@ -9,8 +8,4 @@
 
 TEST(Example, EmptyTest) {
     EXPECT_THROW(example(), std::runtime_error);
-}
-
-TEST(DISABLED_Snapshot, Speen) {
-  for (;;) std::this_thread::yield();
 }
